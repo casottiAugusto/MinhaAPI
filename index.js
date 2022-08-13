dc23f9918b0fdb8b888b767cbb223eafa03b05ba
Joao PC Windos <augusto_casotti@hotmail.com>
2022-08-13 18:07:05 -0300

    Finished editing

--- index.js
+++ index.js
@@ -64,6 +64,40 @@ app.delete('/game/:id', (req, res) => {
 			});
 	}
 });
+app.put("/game/:id",(req,res)=>{
+	if (isNaN(req.params.id)) {
+		res.sendStatus(400);
+	} else {
+		let { id } = req.params;
 
+		db
+			.findOne({ where: { id: id } })
+			.then((g) => {
+				if (g == null) {
+					res.sendStatus(404);
+				} else {
+					const { title, estudio, description, ano } = req.body;
+					if (title != undefined) {
+						g.update({title:title},{where:{id:id}})
+					}
+					if (estudio != undefined) {
+						g.update({estudio:estudio},{where:{id:id}})
+					}
+					if (description != undefined) {
+						g.update({description:description},{where:{id:id}})
+					}
+					if (ano != undefined) {
+						g.update({ano:ano},{where:{id:id}})
+					}
+					res.sendStatus(200)
+				
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+				res.sendStatus(500);
+			});
+		}
+	})
 
 app.listen(8000, (res, req) => {});
